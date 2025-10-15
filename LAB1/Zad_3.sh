c8531f77c0a666cba80664ec37daa34ca297f8a0
Elrior-programmer <284123@student.pwr.edu.pl>
2025-10-15 02:44:24 +0200

    fix zad_3

--- LAB1/Zad_3.sh
+++ LAB1/Zad_3.sh
@@ -7,7 +7,7 @@ for file in "$K"/*; do
     if [ -f "$file" ]; then
         lines=$(wc -l < "$file")
         if [ "$lines" == "0" ]; then
-            if [ -w "$file" ]; then
+            if [ ! -x "$file" ]; then
                 name=$(basename "$file")
                 echo "$name" >> "$P"
                 rm "$file"
